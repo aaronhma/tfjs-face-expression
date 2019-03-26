5151cbbaf35d1a3a1f4a18d5e7237596ac006754
Aaron Ma <aaronhma@gmail.com>
2019-03-26 10:28:12 -0700

    add comments on app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,6 +30,7 @@ export class AppComponent implements OnInit {
   public canvas: any;
 
   ngOnInit() {
+    // access video and canvas HTML DOM element
     this.videoEl = this.video.nativeElement;
     this.canvas = this.overlay.nativeElement;
 
@@ -37,41 +38,52 @@ export class AppComponent implements OnInit {
   }
 
   async run() {
-    // load face detection and face expression recognition models: using 'tiny_face_detector'
+    // load face detection and face expression recognition model named 'tiny_face_detector'
     await faceapi.nets.tinyFaceDetector.load('assets/weights/');
     await faceapi.loadFaceExpressionModel('assets/weights/');
 
     // try to access users webcam and stream the images to the video element
     const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
     this.videoEl.srcObject = stream;
+    // add event listener callback for manipulating webcam streaming data
     this.videoEl.addEventListener('play', this.onPlay.bind(this));
   }
 
+  // manipulating webcam streaming data
   public async onPlay() {
+    // Specify the face detector default options
     const options = new faceapi.TinyFaceDetectorOptions({
+      // size at which image is processed
       inputSize: 512,
+      // minimum confidence threshold: only show successful inference above 50%
       scoreThreshold: 0.5
     });
 
+    // detect the face with the highest confidence score in an image that coming from webcam
+    // `withFaceExpressions`: Face expression recognition is performed for detected face
     const result = await faceapi
       .detectSingleFace(this.videoEl, options)
       .withFaceExpressions();
 
-    // { detection: FaceDetection, expressions: 'neutral', 'angry', 'sad', 'surprised', 'happy', 'disgusted', 'fearful'}
+    // result: { detection: FaceDetection, expressions: 'neutral', 'angry', 'sad', 'surprised', 'happy', 'disgusted', 'fearful'}
     if (result) {
+      // retrieve the detected face dimension
       const { width, height } = faceapi.getMediaDimensions(this.videoEl);
+      // create a bounding box in canvas element overlayed on top of video element
       this.canvas.width = width;
       this.canvas.height = height;
 
-      // resize detections (and landmarks) in case displayed image is smaller than original size
+      // resize detections in case displayed image has a different size then the original
       const resizedResults = faceapi.resizeResults([result], { width, height });
 
+      // the function to draw face detection into a canvas
       faceapi.drawDetection(
         this.canvas,
         resizedResults.map((det: any) => det.detection),
         { withScore: false }
       );
 
+      // the function to draw face expression recognition into a canvas
       faceapi.drawFaceExpressions(
         this.canvas,
         resizedResults.map(({ detection, expressions }) => ({
@@ -81,6 +93,7 @@ export class AppComponent implements OnInit {
       );
     }
 
+    // repeatedly perform the face detection while webcam stream the image data
     setTimeout(() => this.onPlay());
   }
 }
